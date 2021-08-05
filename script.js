097cba67d70ea6a1374cede222a7f3c34b84117e
SebastianLomme <77025210+SebastianLomme@users.noreply.github.com>
2021-08-05 13:48:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function makePizza () {
+    console.log("Maak het deeg");
+    console.log("Maak de saus");
+    console.log("Rol het deeg uit");
+    console.log("Beleg het deeg met saus en kaas");
+    console.log("Voeg extra toppings toe na wens");
+    console.log("Bak de pizza op 300 graden tot deze mooi bruin is");
+    console.log("Eet smakelijk je pizza is klaar");
+}
+
+
+function makeSushi () {
+    console.log("Maak de rijst naar recept");
+    console.log("Snij alle ingredienten");
+    console.log("Roll de sushi");
+    console.log("Snij de sushi in porties");
+}
+
+
+makePizza();
+makePizza();
+
+makeSushi()
+makeSushi()
+makeSushi()
